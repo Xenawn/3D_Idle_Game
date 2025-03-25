3a58e1fdfe18bddfe6bec147a1bdb54410c13004
Xenawn <codeparadoxist@gmail.com>
2025-03-25 17:29:00 +0900

    [Feat] Animatoin Data

---
+++ Assets/Scripts/Player/AnimationHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    
+   // private static readonly int IsMoving = Animator.Strign
+}
---
+++ Assets/Scripts/Player/PlayerAnimationData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationData : MonoBehaviour
+{
+    [SerializeField] private string groundParameterName = "@Ground";
+    [SerializeField] private string idleParameterName = "Idle";
+    [SerializeField] private string walkParameterName = "Walk";
+    [SerializeField] private string runParameterName = "Run";
+
+    public int GroundParameterHash { get; private set; }    
+    public int IdleParameterHash { get; private set; }
+    public int WalkParameterHash { get; private set; }
+    public int RunParameterHash { get; private set; }
+
+    public void Initialize()
+    {
+        GroundParameterHash = Animator.StringToHash(groundParameterName);
+        IdleParameterHash = Animator.StringToHash(idleParameterName);
+        WalkParameterHash = Animator.StringToHash(walkParameterName);
+        RunParameterHash = Animator.StringToHash(runParameterName);
+    }
+}
